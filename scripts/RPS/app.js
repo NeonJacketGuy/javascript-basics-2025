8348ddaaddfbdf989830f9821d7b4d8dd49531d9
Declan Anthony <linko.dman@gamil.com>
2025-09-05 10:22:05 -0600

    The start of the game.

---
+++ scripts/RPS/app.js
@@ -0,0 +1,13 @@
+//@ts-check
+
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+const RAGE = "rage";
+
+
+let aftermathElement = document.getElementById("aftermath");
+
+const pickWeapon = function (weapon) {
+    console.log("Player picked", weapon);
+};
--- scripts/app.js
+++ scripts/app.js
@@ -1,57 +1,10 @@
 //@ts-check
-// This will be our first javascript file;
 
-console.log("Hola world from your basment");
+let two = 2;
+let three = 3;
 
-// this is a string (text) variable
-// strings are surrounded by double quotes "", singles quotes '', or backtick ``;
-let username = "Yes Queennnnnnnnnnnnnnn!!!!!!!!!!!";
-// this variable name is camelCased;
-let luke = "short ginger";
-
-console.log (username, luke);
-
-console.log("luke is a", luke);
-console.log (username, luke);
-
-
-let whathappens = "bob";
-
-let likes = 1000000;
-let sub = 10;
-
-console.log(whathappens);
-
-// booleans are tue false;
-
-let noCap = true;
-let cap = false;
-let is2greaterThan3 = 2 > 3;
-
-console.log(noCap, cap, is2greaterThan3);
-
-
-//define a constant;
-const DO_NOT_CHANGE_ME = "NOT ALLOWED!!!";
-
-//Arrays - lists of stuff;
-
-let foodStuff = ["protien bars", "onian rings", "trail mix"];
-console.log(foodStuff, foodStuff[1]);
-
-let homis = ["Brody", "Brody again because hes cooler than luke", "Declan", "Emma"];
-
-//Objects - this grouped data/info;
-
-let profile = {
-    username: "MeowMeow123",
-    favGame: "The Legend of Zelda: Tears of the Kingdom",
-    homis: ["Brody", "Declan"]
-};
-console.log(profile);
-
-
-profile.likes = 11350138257813759037098;
-
-console.log(profile);
-
+if (two != three) {
+    console.log("Math is broken!");
+ } else {
+    console.log("Sas is broken.");
+ }
