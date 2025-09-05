7628ff23e744fbc30c7824d710ec7717514de2ce
NeonJacketGuy <linko.dman@gamil.com>
2025-09-05 14:10:25 -0600

    Making math smart.

--- scripts/RPS/app.js
+++ scripts/RPS/app.js
@@ -11,3 +11,21 @@ let aftermathElement = document.getElementById("aftermath");
 const pickWeapon = function (weapon) {
     console.log("Player picked", weapon);
 };
+
+
+const selectComputerWeapon = function () {
+    const rand = Math.floor(Math.random() * 4);
+
+    if (rand == 0) {
+        return ROCK;
+    }
+    if (rand == 1) {
+        return PAPER;
+    }
+    if (rand == 2) {
+        return SCISSORS;
+    }
+    if (rand == 3) {
+        return RAGE;
+    }
+};
