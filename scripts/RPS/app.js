fd024141bc0bf068b8f1e3e0aee012c11a957abb
NeonJacketGuy <linko.dman@gamil.com>
2025-09-10 16:23:35 -0600

    wednesday works

--- scripts/RPS/app.js
+++ scripts/RPS/app.js
@@ -4,14 +4,27 @@ const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
 
+/** @type { HTMLElement } */
+//@ts-ignore checking for null below : )
 let aftermathElement = document.getElementById("aftermath");
-
+if (aftermathElement == null) {
+	throw "aftermath is not defined! Go look at your HTML because you made an mistake because you suck!!!";
+}
 const pickWeapon = function (weapon) {
+	let aftermathText = `Player pciked ${weapon}. `;
 	console.log("Player picked", weapon);
 
 	
 	let computerWeapon = selectComputerWeapon();
 	console.log("Computer picked", computerWeapon);
+	aftermathText = aftermathText + `Computer picked ${computerWeapon}. `;
+
+	let results = decideResults(weapon, computerWeapon);
+	console.log("Battle results", results);
+
+	if(results?.isTie) {
+
+	}
 };
 
 const selectComputerWeapon = function () {
@@ -84,4 +97,6 @@ function decideResults(playerWeapon, computerWeapon) {
 		result.description = "Scissors smites down upon paper!";
 		return result;
 	}
-}
+
+	result.description = "2829 PAW PRINT WAY CASTLE ROCK CO 80109!!!";
+};
