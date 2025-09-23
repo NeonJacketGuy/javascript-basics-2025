8f8248166b0432f7109f2624c272f9b5f47395cc
NeonJacketGuy <linko.dman@gamil.com>
2025-09-23 16:06:53 -0600

    AI game

--- scripts/Canvas.js
+++ scripts/Canvas.js
@@ -6,4 +6,8 @@ const canvas = document.getElementById("game-canvas");
 
 const ctx = canvas.getContext("2d");
 
+const HEIGHT = 600;
+const WIDTH = 800;
 
+canvas.height = HEIGHT;
+canvas.width = WIDTH
