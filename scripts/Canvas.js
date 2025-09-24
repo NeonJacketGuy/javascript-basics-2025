498b58a798e71746e9a7cd3346558449b803f628
NeonJacketGuy <linko.dman@gamil.com>
2025-09-24 16:23:29 -0600

    Anamation.

--- scripts/Canvas.js
+++ scripts/Canvas.js
@@ -15,7 +15,7 @@ canvas.height = HEIGHT;
 canvas.width = WIDTH
 
 
-CTX.fillStyle = "orange";
+CTX.fillStyle = "black";
 CTX.fillRect(0, 0, WIDTH, HEIGHT);
 
 CTX.fillStyle = "blue";
@@ -24,13 +24,26 @@ CTX.fillRect(0, 0, 200, 600);
 CTX.fillStyle = "blue";
 CTX.fillRect(600, 0, 200, 600);
 
-CTX.fillStyle = "green";
+CTX.fillStyle = "red";
 CTX.arc(WIDTH / 2, HEIGHT / 2, 100, 0, 2 * Math.PI,);
 CTX.fill();
 
-function drawLoop(timestamp) {
-    console.log(timestamp);
+let currentTimestamp = 0;
+let x = 0;
+let y = 0;
 
+function drawLoop(timestamp) {
+    //CTX.clearRect(0, 0, WIDTH, HEIGHT)
+    let elapsedTime = timestamp - currentTimestamp;
+    currentTimestamp = timestamp;
+    
+    CTX.fillStyle = "green";
+    CTX.fillRect(x, y, 10, 10);
+
+    x++;
+    y++;
+
+    //console.log(elapsedTime);
     requestAnimationFrame(drawLoop);
 }
 
