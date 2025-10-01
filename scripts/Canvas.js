c6159a42cb6653a48c032d65040240f5ebaeb6d5
NeonJacketGuy <linko.dman@gamil.com>
2025-10-01 15:41:43 -0600

    Canvas.js update

--- scripts/Canvas.js
+++ scripts/Canvas.js
@@ -14,8 +14,6 @@ const WIDTH = 800;
 CANVAS.height = HEIGHT;
 CANVAS.width = WIDTH;
 
-
-
 class Box {
 	constructor(x, y, color) {
 		this.x = x;
@@ -62,95 +60,28 @@ class Box {
 	}
 }
 
+/** @type { Box[] } */
+let boxes = [];
+
+for (let i = 0; i <= 10000; i++) {
+	let box = new Box(WIDTH / 2, HEIGHT / 2, "black");
+	box.width = 2;
+	box.height = 10;
+	box.x = Math.random() * (WIDTH - 100);
+	box.y = Math.random() * (HEIGHT - 100);
+	box.speed = Math.random() * 5 + 5;
+	boxes.push(box);
+}
+
 let currentTimestamp = 0;
-
-let box = {
-	x: 0,
-	y: 0,
-	speed: 10,
-	xDirection: 1,
-	yDirection: 1,
-	width: 50,
-	draw: function () {
-		CTX.fillStyle = "black";
-		CTX.fillRect(this.x, this.y, this.width, this.width);
-	},
-	update: function () {
-		let top = this.y;
-		let bottom = this.y + this.width;
-		let left = this.x;
-		let right = this.x + this.width;
-
-		if (top < 0) {
-			// colliding with top
-			this.yDirection = 1;
-		} else if (bottom > HEIGHT) {
-			// colliding with bottom
-			this.yDirection = -1;
-		}
-
-		if (left < 0) {
-			// colliding with left
-			this.xDirection = 1;
-		} else if (right > WIDTH) {
-			// colliding with right
-			this.xDirection = -1;
-		}
-
-		this.x += this.xDirection * this.speed;
-		this.y += this.yDirection * this.speed;
-	},
-};
-
-let box2 = {
-	x: 100,
-	y: 50,
-	speed: 15,
-	xDirection: -1,
-	yDirection: 1,
-	width: 50,
-	draw: function () {
-		CTX.fillStyle = "purple";
-		CTX.fillRect(this.x, this.y, this.width, this.width);
-	},
-	update: function () {
-		let top = this.y;
-		let bottom = this.y + this.width;
-		let left = this.x;
-		let right = this.x + this.width;
-
-		if (top < 0) {
-			// colliding with top
-			this.yDirection = 1;
-		} else if (bottom > HEIGHT) {
-			// colliding with bottom
-			this.yDirection = -1;
-		}
-
-		if (left < 0) {
-			// colliding with left
-			this.xDirection = 1;
-		} else if (right > WIDTH) {
-			// colliding with right
-			this.xDirection = -1;
-		}
-
-		this.x += this.xDirection * this.speed;
-		this.y += this.yDirection * this.speed;
-	},
-};
 
 function drawLoop(timestamp) {
 	CTX.clearRect(0, 0, WIDTH, HEIGHT);
 
-	let elapsedTime = timestamp - currentTimestamp;
-	currentTimestamp = timestamp;
-
-	box.draw();
-	box.update();
-
-	box2.draw();
-	box2.update();
+	boxes.forEach((b) => {
+		b.draw();
+		b.update();
+	});
 
 	// console.log(elapsedTime);
 	requestAnimationFrame(drawLoop);
